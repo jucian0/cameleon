2389dce93edac7849a5a9ee89c56300243bf1628
Jucian0 <juciano@outlook.com.br>
2025-07-01 22:56:39 -0300

    vercel deploying

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/react-router/vite";
+
 declare module "react-router" {
   interface Future {
     unstable_middleware: true; // 👈 Enable middleware types
@@ -9,5 +11,6 @@ export default {
   ssr: true,
   future: {
     unstable_middleware: true,
-  }
+  },
+  presets: [vercelPreset()],
 } satisfies Config;
