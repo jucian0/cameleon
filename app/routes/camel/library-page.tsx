7daaed54e2c1bbb4fa021a77a9699c65c3c4bd89
Jucian0 <juciano@outlook.com.br>
2025-07-13 17:51:34 -0300

    adding eips and components routes

--- app/routes/camel/library-page.tsx
+++ app/routes/camel/library-page.tsx
@@ -1,7 +1,7 @@
 import { Autocomplete, useFilter } from "react-aria-components";
 import { Tabs } from "components/ui/tabs";
 import { SearchField } from "components/ui/search-field";
-import { Outlet, useLocation } from "react-router";
+import { Outlet, useLocation, useSearchParams } from "react-router";
 
 
 export default function TopologyLibrary() {
@@ -9,9 +9,17 @@ export default function TopologyLibrary() {
   const routeParts = pathname.split("/");
   const currentCamelRoute = pathname.split("/")[routeParts.length - 1];
 
-  console.log("Current Camel Route:", currentCamelRoute);
-
+  const [filter, setFilter] = useSearchParams();
   const { contains } = useFilter({ sensitivity: 'base' });
+
+  function handleQueryChange(value: string) {
+    if (value) {
+      setFilter({ q: value });
+    } else {
+      setFilter({});
+    }
+  }
+
   return (
     <div className="m-6 flex flex-col gap-4">
       <Autocomplete aria-label="Topology library" filter={contains}>
@@ -20,12 +28,20 @@ export default function TopologyLibrary() {
             aria-label="Search by name"
             placeholder="Search"
             className="w-full"
+            defaultValue={filter.get('q') || ''}
+            onChange={handleQueryChange}
           />
         </div>
         <Tabs aria-label="Camel EIPs and Components" selectedKey={currentCamelRoute}>
           <Tabs.List>
-            <Tabs.Tab id="eips" href="/camel/library/eips">EIPs</Tabs.Tab>
-            <Tabs.Tab id="components" href="/camel/library/components">Components</Tabs.Tab>
+            <Tabs.Tab id="eips" href="/camel/library/eips" routerOptions={{
+              replace: true,
+              state: { scrollToTop: true }
+            }}>EIPs</Tabs.Tab>
+            <Tabs.Tab id="components" href="/camel/library/components" routerOptions={{
+              replace: true,
+              state: { scrollToTop: true }
+            }}>Components</Tabs.Tab>
           </Tabs.List>
           <Tabs.Panel id="eips">
           </Tabs.Panel>
