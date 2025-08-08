8cd8fab6bd1ebdde4a55de136a778b9196a1d4cb
Jucian0 <juciano@outlook.com.br>
2025-08-07 22:35:55 -0300

    error deploying on vercel

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -5,22 +5,24 @@ import { Outlet, redirect } from "react-router";
 import type { Route } from "./+types/layout";
 import { createServerSupabase } from "@/modules/supabase/supabase-server";
 
-const serverAuth: Route.unstable_MiddlewareFunction = async ({ request }) => {
-  const { supabase } = createServerSupabase(request);
-  const { data, error } = await supabase.auth.getUser();
-  if (!data.user || error) {
-    throw redirect("/auth");
-  }
-};
+// const serverAuth: Route.unstable_MiddlewareFunction = async ({ request }) => {
+//   const { supabase } = createServerSupabase(request);
+//   const { data, error } = await supabase.auth.getUser();
+//   if (!data.user || error) {
+//     throw redirect("/auth");
+//   }
+// };
 
-export const unstable_middleware = [serverAuth]
+// export const unstable_middleware = [serverAuth]
 
 export default function Layout() {
-  return (<SidebarProvider>
-    <AppSidebar collapsible="dock" />
-    <SidebarInset>
-      <AppSidebarNav />
-      <Outlet />
-    </SidebarInset>
-  </SidebarProvider>)
+  return (
+    <SidebarProvider>
+      <AppSidebar collapsible="dock" />
+      <SidebarInset>
+        <AppSidebarNav />
+        <Outlet />
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
