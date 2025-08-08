eca9ff51a590ce54875405b8560906793f826eb9
Jucian0 <juciano@outlook.com.br>
2025-08-07 22:35:55 -0300

    error deploying on vercel

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,14 +1,14 @@
 import type { Config } from "@react-router/dev/config";
 
-declare module "react-router" {
-  interface Future {
-    unstable_middleware: true; // 👈 Enable middleware types
-  }
-}
+// declare module "react-router" {
+//   interface Future {
+//     unstable_middleware: true; // 👈 Enable middleware types
+//   }
+// }
 
 export default {
   ssr: true,
-  future: {
-    unstable_middleware: true,
-  },
+  // future: {
+  //   unstable_middleware: true,
+  // },
 } satisfies Config;
