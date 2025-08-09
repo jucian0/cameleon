2afde5c24d86450e137f233e283f5ced302ed05a
Jucian0 <juciano@outlook.com.br>
2025-08-09 19:16:45 -0300

    updating pages

--- app/routes.ts
+++ app/routes.ts
@@ -1,19 +1,25 @@
-import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  prefix,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
-  route('auth', 'routes/auth/auth-page.tsx'),
-  route('auth/callback', 'routes/auth/callback-page.ts'),
-  layout('routes/layout.tsx', [
-    index("routes/home/home-page.tsx"),
-    ...prefix('camel', [
-      route('studio', 'routes/camel/studio-page.tsx'),
-      route('library', 'routes/camel/library-page.tsx', [
+  route("auth", "pages/auth/auth-page.tsx"),
+  route("auth/callback", "pages/auth/callback-page.ts"),
+  layout("pages/layout.tsx", [
+    index("pages/home/home-page.tsx"),
+    ...prefix("camel", [
+      route("studio", "pages/camel/studio-page.tsx"),
+      route("library", "pages/camel/library-page.tsx", [
         // index('routes/camel/library-eips-page.tsx'),
-        route('eips', 'routes/camel/library-eips-page.tsx'),
-        route('components', 'routes/camel/library-components-page.tsx'),
-        route('presets', 'routes/camel/library-presets-page.tsx'),
+        route("eips", "pages/camel/library-eips-page.tsx"),
+        route("components", "pages/camel/library-components-page.tsx"),
+        route("presets", "pages/camel/library-presets-page.tsx"),
       ]),
     ]),
-    route('set-theme', 'routes/set-theme/set-theme-page.ts'),
+    route("set-theme", "pages/set-theme/set-theme-page.ts"),
   ]),
 ] satisfies RouteConfig;
