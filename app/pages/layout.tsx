b812e38dd8d4745683703831037e2ea02f805aa3
Jucian0 <juciano@outlook.com.br>
2025-08-11 12:40:01 -0300

    update auth system

--- app/pages/layout.tsx
+++ app/pages/layout.tsx
@@ -1,19 +1,17 @@
 import AppSidebar from "components/app-sidebar";
 import AppSidebarNav from "components/app-sidebar-nav";
 import { SidebarProvider, SidebarInset } from "components/ui/sidebar";
-import { Outlet, redirect } from "react-router";
-import type { Route } from "./+types/layout";
+import { Outlet, redirect, type LoaderFunctionArgs } from "react-router";
 import { createServerSupabase } from "@/modules/supabase/supabase-server";
 
-// const serverAuth: Route.unstable_MiddlewareFunction = async ({ request }) => {
-//   const { supabase } = createServerSupabase(request);
-//   const { data, error } = await supabase.auth.getUser();
-//   if (!data.user || error) {
-//     throw redirect("/auth");
-//   }
-// };
-
-// export const unstable_middleware = [serverAuth]
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { supabase } = createServerSupabase(request);
+  const { data, error } = await supabase.auth.getUser();
+  if (!data.user || error) {
+    throw redirect("/auth");
+  }
+  return null;
+}
 
 export default function Layout() {
   return (
