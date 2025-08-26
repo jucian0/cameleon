b23cb219656ebd7c1fc39f6493056aafb9b3f2b8
Jucian0 <juciano@outlook.com.br>
2025-08-26 17:37:59 -0300

    rename project camelion to cameleon

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -1,11 +1,17 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  build: {
-    outDir: "build",
-  },
+export default defineConfig((config) => {
+  const env = loadEnv(config.mode, process.cwd(), "");
+  return {
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    build: {
+      outDir: "build",
+    },
+    define: {
+      "process.env": env,
+    },
+  };
 });
