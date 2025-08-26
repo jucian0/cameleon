1ae8fa6e4101a5b76cd8f569ec5308f4fb521320
Jucian0 <juciano@outlook.com.br>
2025-08-26 18:04:39 -0300

    updating env config

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -1,17 +1,11 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig((config) => {
-  const env = loadEnv(config.mode, process.cwd(), "");
-  return {
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-    build: {
-      outDir: "build",
-    },
-    define: {
-      "process.env": env,
-    },
-  };
+export default defineConfig({
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  build: {
+    outDir: "build",
+  },
 });
