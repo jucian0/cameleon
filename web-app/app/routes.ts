cf4870ed77e92a96f1e767328cd5ce6f9b8be1a4
Jucian0 <juciano@outlook.com.br>
2025-09-22 22:00:44 -0300

    test

--- web-app/app/routes.ts
+++ web-app/app/routes.ts
@@ -1,74 +1,81 @@
-import {
-  type RouteConfig,
-  index,
-  layout,
-  prefix,
-  route,
-} from "@react-router/dev/routes";
+// import {
+//   type RouteConfig,
+//   index,
+//   layout,
+//   prefix,
+//   route,
+// } from "@react-router/dev/routes";
 
-export default [
-  route("", "pages/landing/page.tsx"),
+// export default [
+//   route("", "pages/landing/page.tsx"),
 
-  ...prefix("app", [
-    route("auth", "pages/app/auth/auth-page.tsx"),
-    route("auth/callback", "pages/app/auth/callback-page.ts"),
-    layout("pages/app/layout.tsx", [
-      index("pages/app/home/home-page.tsx"),
-      ...prefix("camel", [
-        route("studio", "pages/app/camel/workflows-page/studio-page/page.tsx", {
-          id: "workflows-create",
-        }),
-        route(
-          "workflows/:workflowsId/studio",
-          "pages/app/camel/workflows-page/studio-page/page.tsx",
-          {
-            id: "workflow-edit",
-          },
-          // [
-          //   // route(
-          //   //   "edit",
-          //   //   "pages/app/camel/workflows-page/studio-page/page.tsx",
-          //   // ),
-          //   // route("replace", ""),
-          //   // route("add-next", ""),
-          //   // route("add-between", ""),
-          // ],
-        ),
-        route("workflows", "pages/app/camel/workflows-page/page.tsx", [
-          route(
-            "create",
-            "pages/app/camel/workflows-page/details-page/page.tsx",
-            {
-              id: "studio-create",
-            },
-          ),
-          route(
-            ":workflowsId/edit",
-            "pages/app/camel/workflows-page/details-page/page.tsx",
-            {
-              id: "studio-edit",
-            },
-          ),
-          route(
-            ":workflowsId/clone",
-            "pages/app/camel/workflows-page/details-page/page.tsx",
-            {
-              id: "studio-clone",
-            },
-          ),
-        ]),
-        ...prefix("library", [
-          layout("pages/app/camel/library-page/layout.tsx", [
-            route("eips", "pages/app/camel/library-page/eips-page.tsx"),
-            route(
-              "components",
-              "pages/app/camel/library-page/components-page.tsx",
-            ),
-            route("presets", "pages/app/camel/library-page/presets-page.tsx"),
-          ]),
-        ]),
-      ]),
-      route("set-theme", "pages/app/set-theme/set-theme-page.ts"),
-    ]),
-  ]),
-] satisfies RouteConfig;
+//   ...prefix("app", [
+//     route("auth", "pages/app/auth/auth-page.tsx"),
+//     route("auth/callback", "pages/app/auth/callback-page.ts"),
+//     layout("pages/app/layout.tsx", [
+//       index("pages/app/home/home-page.tsx"),
+//       ...prefix("camel", [
+//         route("studio", "pages/app/camel/workflows-page/studio-page/page.tsx", {
+//           id: "workflows-create",
+//         }),
+//         route(
+//           "workflows/:workflowsId/studio",
+//           "pages/app/camel/workflows-page/studio-page/page.tsx",
+//           {
+//             id: "workflow-edit",
+//           },
+//           // [
+//           //   // route(
+//           //   //   "edit",
+//           //   //   "pages/app/camel/workflows-page/studio-page/page.tsx",
+//           //   // ),
+//           //   // route("replace", ""),
+//           //   // route("add-next", ""),
+//           //   // route("add-between", ""),
+//           // ],
+//         ),
+//         route("workflows", "pages/app/camel/workflows-page/page.tsx", [
+//           route(
+//             "create",
+//             "pages/app/camel/workflows-page/details-page/page.tsx",
+//             {
+//               id: "studio-create",
+//             },
+//           ),
+//           route(
+//             ":workflowsId/edit",
+//             "pages/app/camel/workflows-page/details-page/page.tsx",
+//             {
+//               id: "studio-edit",
+//             },
+//           ),
+//           route(
+//             ":workflowsId/clone",
+//             "pages/app/camel/workflows-page/details-page/page.tsx",
+//             {
+//               id: "studio-clone",
+//             },
+//           ),
+//         ]),
+//         ...prefix("library", [
+//           layout("pages/app/camel/library-page/layout.tsx", [
+//             route("eips", "pages/app/camel/library-page/eips-page.tsx"),
+//             route(
+//               "components",
+//               "pages/app/camel/library-page/components-page.tsx",
+//             ),
+//             route("presets", "pages/app/camel/library-page/presets-page.tsx"),
+//           ]),
+//         ]),
+//       ]),
+//       route("set-theme", "pages/app/set-theme/set-theme-page.ts"),
+//     ]),
+//   ]),
+// ] satisfies RouteConfig;
+
+import { type RouteConfig } from "@react-router/dev/routes";
+import { flatRoutes } from "@react-router/fs-routes";
+
+export default flatRoutes({
+  rootDirectory: "routes",
+}) satisfies RouteConfig;
---
+++ web-app/app/routes/app.tsx
@@ -0,0 +1,26 @@
+import AppSidebar from "app/components/app-sidebar";
+import AppSidebarNav from "app/components/app-sidebar-nav";
+import { SidebarProvider, SidebarInset } from "app/components/ui/sidebar";
+import { Outlet, redirect, type LoaderFunctionArgs } from "react-router";
+import { createServerSupabase } from "@/modules/supabase/supabase-server";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { supabase } = createServerSupabase(request);
+  const { data, error } = await supabase.auth.getUser();
+  if (!data.user || error) {
+    throw redirect("/app/auth");
+  }
+  return null;
+}
+
+export default function Layout() {
+  return (
+    <SidebarProvider>
+      <AppSidebar collapsible="dock" />
+      <SidebarInset>
+        <AppSidebarNav />
+        <Outlet />
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
